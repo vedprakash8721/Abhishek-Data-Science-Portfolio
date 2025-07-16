fa92f23f6b2991bbe5a436c835dbc665b3714b34
Ved <askrocks2001@gmail.com>
2025-07-16 17:16:33 +0530

    Alter: add column to the existing table

--- SQL/Alter.sql
+++ SQL/Alter.sql
@@ -1,1 +1,13 @@
-#
+-- to change the schema 
+# alter is used to add , rename and to drop the columm
+use cricket;
+
+-- to add column
+alter table Players
+add column career int ;
+select * from Players;
+
+# adding some value to see how it looks
+update Players
+set career=16 where Cap_num=101;
+set sql_safe_updates=0;
