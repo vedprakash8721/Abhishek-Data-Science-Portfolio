3f1ddddd934e0ea71a82ba120cd5fe59923c4563
Ved <askrocks2001@gmail.com>
2025-07-17 22:11:40 +0530

    Alter : drop column

--- SQL/Alter.sql
+++ SQL/Alter.sql
@@ -11,3 +11,13 @@ select * from Players;
 update Players
 set career=16 where Cap_num=101;
 set sql_safe_updates=0;
+
+-- we can use it for deleting the column 
+# we don't want to delete existing data so we will do this
+alter table Players
+add column yuhi varchar(5);
+select * from Players;
+
+# now delete the column
+alter table Players
+drop column yuhi;
